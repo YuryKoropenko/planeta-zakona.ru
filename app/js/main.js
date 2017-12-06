157b2cfddde5266a9e1c9b4aa372156fda5342f0
YuryKoropenko <yurykoropenko@gmail.com>
2017-12-06 17:28:09 +0200

    шапка (со скриптами) + начало контент части

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,38 @@
 $(function() {
-	
+
+	/*Главное меню (header)*/
+	$('.h-nav__item-menu-1').mouseover(function() {
+		$(this).addClass('h-nav__item-active');
+		$('.h-subnav-1').show();
+	});
+
+	/*email-рассылка*/
+	$('.h-email__link-button').on('click', function() {
+		$('.header__h-nav').toggle();
+		$('.header__h-wrapper').toggle();
+		$('.header__h-dispatch').toggle();
+		return false;
+	});
+	$('.h-dispatch__close').on('click', function() {
+		$('.header__h-dispatch').hide();
+		$('.header__h-nav').show();
+		$('.header__h-wrapper').show();
+		return false;
+	});
+
+
+	/*поиск по сайту*/
+	$('.h-search-button').on('click', function() {
+		$('.header__h-nav').toggle();
+		$('.header__h-wrapper').toggle();
+		$('.header__h-sitesearch').toggle();
+		return false;
+	});
+	$('.h-dispatch__close').on('click', function() {
+		$('.header__h-sitesearch').hide();
+		$('.header__h-nav').show();
+		$('.header__h-wrapper').show();
+		return false;
+	});
+
 });
